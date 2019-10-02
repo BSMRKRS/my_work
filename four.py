3a3ddd40b479eddf8f60e53afd4424ff6f392719
dbruer22 <dbruer22@bsmschool.org>
2019-10-02 13:05:10 -0500

    general save

---
+++ four.py
@@ -0,0 +1,17 @@
+import RoboPiLib as RPL
+import setup
+import time
+x = 1
+sensor_pin = 16
+RPL.pinMode(sensor_pin,RPL.INPUT)
+while x == 1:
+    reading = RPL.digitalRead(sensor_pin)
+    if reading == 0:
+        RPL.servoWrite(1,1400)
+        time.sleep(.2)
+        RPL.servoWrite(1,1400)
+        time.sleep(.2)
+        RPL.servoWrite(1,1400)
+        time.sleep(.2)
+    elif reading == 1:
+        RPL.servoWrite(1,0)
