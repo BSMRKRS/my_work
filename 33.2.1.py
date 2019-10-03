6d2765b79aabac323525dd2e6470fa86b87acd20
rlong22 <rlong22@bsmschool.org>
2019-10-03 13:03:33 -0500

    general save

--- 33.2.1.py
+++ 33.2.1.py
@@ -1,25 +1,12 @@
 import RoboPiLib as RPL
 import setup
 x = 1
-sensor_pin = 2
-RPL.pin(2,RPL.INPUT)
+sensor_pin = 0
+RPL.pinMode(0,RPL.INPUT)
 while x == 1:
-  reading = RPL.digitalRead(2)
+  reading = RPL.digitalRead(0)
   if reading == 0:
     RPL.servoWrite(0, 1590)
   elif reading == 1:
     RPL.servoWrite(0, 1500)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
