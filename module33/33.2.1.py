dba0ec421dc75d2fbf30428b6ed9493146bb15ec
jborin21 <jborin21@bsmschool.org>
2019-10-04 10:59:57 -0500

    general save

--- module33/33.2.1.py
+++ module33/33.2.1.py
@@ -1,12 +1,12 @@
 import RoboPiLib as RPL
 import setup
 x = 1
-sensor_pin = 0
-RPL.pinMode(0,RPL.INPUT)
+sensor_pin = 16 
+RPL.pinMode(sensor_pin ,RPL.INPUT)
 while x == 1:
-  reading = RPL.digitalRead(0)
+  reading = RPL.digitalRead(sensor_pin)
   if reading == 0:
-    RPL.servoWrite(0, 1590)
+    RPL.servoWrite(0, 1400) 
   elif reading == 1:
     RPL.servoWrite(0, 1500)
 
---
+++ module33/randomeLine.py
@@ -0,0 +1,14 @@
+import RoboPiLib as RPL
+import setup
+sensor_pin = 16
+sensor_pin2 = 17
+running = True
+RPL.pinMode(sensor_pin,RPL.INPUT)
+RPL.pinMode(sensor_pin2,RPL.INPUT)
+while running:
+	reading = RPL.digitalRead(sensor_pin)
+	reading2 = RPL.digitalRead(sensor_pin2)
+	if reading == 0 or reading2 == 0:
+		RPL.servoWrite(0,1400)
+	elif reading == 1 and reading2 == 1:
+		RPL.servoWrite(0,1500)
