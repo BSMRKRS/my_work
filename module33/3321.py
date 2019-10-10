b9907f324a2e04406023e2639e953a85562c1dbc
ajimenez21 <@bsmschool.org>
2019-10-10 10:05:15 -0500

    general save

---
+++ module33/3321.py
@@ -0,0 +1,10 @@
+import RoboPiLib as RPL
+import setup
+
+sensor_pin = 17     
+value = RPL.pinMode(17,RPL.INPUT)
+ 
+if RPL.digitalRead(17) == 0:
+	RPL.servoWrite(1,1400)
+else:
+	RPL.servoWrite(1,1500)  
---
+++ module33/3332.py
@@ -0,0 +1,25 @@
+import RoboPiLib as RPL 
+import setup 
+import time
+
+while True:
+    RPL.servoWrite(0,1400)
+    RPL.servoWrite(1,1400)
+    time.sleep(2)
+    RPL.servoWrite(0,1500) 
+    time.sleep(1) 
+    RPL.servoWrite(1,1500)
+    time.sleep(1) 
+    RPL.servoWrite(0,1400)
+    time.sleep(2)
+    RPL.servoWrite(1,1400)
+    RPL.servoWrite(0,1500)
+    time.sleep(2) 
+    RPL.servoWrite(0,1400)
+    time.sleep(1) 
+    RPL.servoWrite(1,1500) 
+    time.sleep(1) 
+    RPL.servoWrite(0,1500)
+    time.sleep(2)
+    
+
