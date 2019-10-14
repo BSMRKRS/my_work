2232c3f93573af83f2c70957e7ba59896fd249c9
ajimenez21 <ajimenez21@bsmschool.org>
2019-10-14 09:29:20 -0500

    general save

--- module33/3332.py
+++ module33/3332.py
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import RoboPiLib as RPL
-import setup
-import time
-from multiprocessing import Process 
-x = RPL.servoWrite(0,1400) 
-y = RPL.servoWrite(1,1400) 
- 
-def wheel_1(): 
-	while True:
-		RPL.servoWrite(0,1400) 
-		time.sleep(2)
-		RPL.servoWrite(0,1500)
-		time.sleep(2)	
-def wheel_2():
-	while True:
-		RPL.servoWrite(1,1400) 
-		time.sleep(3)
-		RPL.servoWrite(1,1500)
-		time.sleep(3) 
-
-if True:
-	Process(target=wheel_1).start()
-	Process(target=wheel_2).start()
-=======
-=======
->>>>>>> master
 import RoboPiLib as RPL 
 import setup 
 import time
@@ -50,8 +22,3 @@ while True:
     RPL.servoWrite(0,1500)
     time.sleep(2)
     
-
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> master
--- module33/3341.py
+++ module33/3341.py
@@ -1,11 +1,13 @@
 import RoboPiLib as RPL 
-import setup 
-import time
+import setup
+import time 
+sensor_pin = 16
 
-while True:
-    RPL.servoWrite(0,1400)
-    RPL.servoWrite(1,1400)
-    time.sleep(2)
-    RPL.servoWrite(0,1500) 
-    time.sleep(1) 
-    RPL.servoWrite(1,1500)
+for number in range(1):
+	if RPL.pinMode(sensor_pin,RPL.INPUT) == 0:
+    		RPL.servoWrite(0,1400)
+    		time.sleep(2)
+    		RPL.servoWrite(0,1500) 
+    		time.sleep(1) 
+	else:
+		RPL.servoWrite(0,1500) 
