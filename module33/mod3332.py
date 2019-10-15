4fbc418caf6e820421d8510a0411744ef62c7cbb
ljimenez21 <ljimenez21@bsmschool.org>
2019-10-15 09:24:33 -0500

    general save

--- module33/mod3332.py
+++ module33/mod3332.py
@@ -2,6 +2,7 @@ import RoboPiLib as RPL
 import setup
 import time
 
+<<<<<<< Updated upstream
 i = 1
 while i < 5:
 	i += 1
@@ -17,3 +18,16 @@ while i < 5:
 	RPl.servoWrite(0,1500)
 	RPL.servoWrite(1,1500)
 	time.sleep(1)
+=======
+def motorPulse(motor,speed,seconds):
+	i = 1
+	while i < 5:
+		i += 1
+		RPL.servoWrite(motor,speed)
+		time.sleep(seconds)
+		RPL.servoWrite(motor,1500)
+		time.sleep(seconds)
+
+motorPulse(0,1400,1)
+motorPulse(1,1600,2)
+>>>>>>> Stashed changes
