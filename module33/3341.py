ce97f419ae039039f55f53275d3dd02028afff50
ajimenez21 <56120820+ajimenez21@users.noreply.github.com>
2019-10-22 09:12:35 -0500

    Update 3341.py

--- module33/3341.py
+++ module33/3341.py
@@ -4,19 +4,20 @@ import time
 sensor_pin = 16
 x = 1
 
-for x in range(1000):
-	if RPL.digitalRead(16) == 0:
-		RPL.servoWrite(0,1400)
-		time.sleep(0.5)
-		RPL.servoWrite(0,1500)
-		time.sleep(0.5)
-		RPL.servoWrite(0,1400)
-		time.sleep(0.5)
-		RPL.servoWrite(0,1500)
-		time.sleep(0.5)
-		RPL.servoWrite(0,1400)
-		time.sleep(0.5)
-		RPL.servoWrite(0,1500)
-	else:
-		break
-	x += 1
+while True:
+	for x in range(1000):
+		if RPL.digitalRead(16) == 0:
+			RPL.servoWrite(0,1400)
+			time.sleep(0.5)
+			RPL.servoWrite(0,1500)
+			time.sleep(0.5)
+			RPL.servoWrite(0,1400)
+			time.sleep(0.5)
+			RPL.servoWrite(0,1500)
+			time.sleep(0.5)
+			RPL.servoWrite(0,1400)
+			time.sleep(0.5)
+			RPL.servoWrite(0,1500)
+		else:
+			break
+		x += 1
