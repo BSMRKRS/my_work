c9dd3f2244bc33c4b37524cc0e858317a935f037
ajimenez21 <56120820+ajimenez21@users.noreply.github.com>
2019-10-22 08:55:16 -0500

    Update 3341.py

--- module33/3341.py
+++ module33/3341.py
@@ -3,11 +3,16 @@ import setup
 import time 
 sensor_pin = 16
 
-for number in range(1):
-	if RPL.pinMode(sensor_pin,RPL.INPUT) == 0:
-    		RPL.servoWrite(0,1400)
-    		time.sleep(2)
-    		RPL.servoWrite(0,1500) 
-    		time.sleep(1) 
-	else:
-		RPL.servoWrite(0,1500) 
+def pulse():
+	for number in range(3):
+		if RPL.pinMode(sensor_pin,RPL.INPUT) == 0:
+    			RPL.servoWrite(0,1400)
+    			time.sleep(1)
+    			RPL.servoWrite(0,1500) 
+    			time.sleep(1) 
+		else:
+			RPL.servoWrite(0,1500) 
+
+while True:
+	def pulse():
+		
