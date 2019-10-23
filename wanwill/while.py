fd07d366c3ee66b280fdbb1335ef35641a65dd95
OHoerner21 <@bsmschool.org>
2019-10-23 10:46:26 -0500

    general save

--- wanwill/while.py
+++ wanwill/while.py
@@ -7,8 +7,9 @@ readingR = RPL.digitalRead(R)
 x = 0
 if readingR == 0:
 	while x < 10:
-		RPL.servoWrite(0,1400)
+		RPL.servoWrite(0,1600)
 		time.sleep(1)
 		RPL.servoWrite(0,0)
 		time.sleep(1)
 		x = x + 1
+
