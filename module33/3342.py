21e680e56a462c24a52448f25225c90ca7fd0516
ajimenez21 <56120820+ajimenez21@users.noreply.github.com>
2019-10-24 08:15:38 -0500

    Update 3342.py

--- module33/3342.py
+++ module33/3342.py
@@ -4,21 +4,20 @@ import time
 sensor_pin = 16
 x = 1
 
-while True:
-	for x in range(20):
-		RPL.pinMode(sensor_pin,RPL.INPUT)
-		if RPL.digitalRead(16) == 0:
-			RPL.servoWrite(0,1400)
-			time.sleep(0.5)
-			RPL.servoWrite(0,1500)
-			time.sleep(0.5)
-			RPL.servoWrite(0,1400)
-			time.sleep(0.5)
-			RPL.servoWrite(0,1500)
-			time.sleep(0.5)
-			RPL.servoWrite(0,1400)
-			time.sleep(0.5)
-			RPL.servoWrite(0,1500)
-		else:
-			time.sleep(0.5)
-		x += 1
+for x in range(20):
+	RPL.pinMode(sensor_pin,RPL.INPUT)
+	if RPL.digitalRead(16) == 0:
+		RPL.servoWrite(0,1400)
+		time.sleep(0.5)
+		RPL.servoWrite(0,1500)		
+		time.sleep(0.5)
+		RPL.servoWrite(0,1400)
+		time.sleep(0.5)
+		RPL.servoWrite(0,1500)			
+		time.sleep(0.5)
+		RPL.servoWrite(0,1400)
+		time.sleep(0.5)
+		RPL.servoWrite(0,1500)
+	else:
+		time.sleep(0.5)
+	x += 1
