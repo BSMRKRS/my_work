5a3cdcf472b7b89bbf932995c463af37359c14e5
ljimenez21 <ljimenez21@bsmschool.org>
2019-10-24 09:17:47 -0500

    general save

--- module33/mod3351.py
+++ module33/mod3351.py
@@ -1,4 +1,17 @@
 import RoboPiLib as RPL
 import setup
+import time
 
-print(RPL.analogRead(1))
+i = 0
+
+while i < 15:
+	i += 1
+	reading = RPL.analogRead(1)
+	print(reading)
+	if reading < 200:
+		RPL.servoWrite(0,1400)
+		RPL.servoWrite(1,1600)	
+	else:
+		RPL.servoWrite(0,1500)
+		RPL.servoWrite(1,1500)
+	time.sleep(0.5)
