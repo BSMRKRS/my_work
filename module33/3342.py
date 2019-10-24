d377f439075cf4a020b28a463689312650565e0e
ajimenez21 <56120820+ajimenez21@users.noreply.github.com>
2019-10-24 08:13:25 -0500

    Update 3342.py

--- module33/3342.py
+++ module33/3342.py
@@ -1,1 +1,24 @@
-Import RoboPiLib as RPL
+import RoboPiLib as RPL 
+import setup
+import time 
+sensor_pin = 16
+x = 1
+
+while True:
+	for x in range(20):
+		RPL.pinMode(sensor_pin,RPL.INPUT)
+		if RPL.digitalRead(16) == 0:
+			RPL.servoWrite(0,1400)
+			time.sleep(0.5)
+			RPL.servoWrite(0,1500)
+			time.sleep(0.5)
+			RPL.servoWrite(0,1400)
+			time.sleep(0.5)
+			RPL.servoWrite(0,1500)
+			time.sleep(0.5)
+			RPL.servoWrite(0,1400)
+			time.sleep(0.5)
+			RPL.servoWrite(0,1500)
+		else:
+			time.sleep(0.5)
+		x += 1
