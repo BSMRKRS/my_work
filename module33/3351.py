a2bccc3b5ad5bf31f16eca2272b275c64e4ba226
ajimenez21 <56120820+ajimenez21@users.noreply.github.com>
2019-10-28 08:57:11 -0500

    Update 3351.py

--- module33/3351.py
+++ module33/3351.py
@@ -1,2 +1,9 @@
 import RoboPiLib as RPL
 import setup
+While True:
+  while RPL.analogRead(8) < 500:
+    RPL.servoWrite(0,1400)
+    RPL.servoWrite(1,1400)
+  else:
+    RPL.servoWrite(0,1450)
+    RPL.servoWrite(1,1450)
